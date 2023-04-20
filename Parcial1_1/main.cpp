4c4ef8ebc5eaf641ddaba5d3355324b356fc26e5
fedefpatin <fedefpatino@gmail.com>
2023-04-19 22:08:20 -0500

    Agregando arreglos2

--- Parcial1_1/main.cpp
+++ Parcial1_1/main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 int main()
 {
-    char letra,n;
+    char letra;
+    int n=1;
+
     char array[n],code[n],name[n],credits[n],days[n],schedule[n];
 
+    cout<<code<<endl;
 
 
     return 0;
