04a3ab24bd32cfa1e5624bd41fa0d2d117a457f7
fedefpatin <fedefpatino@gmail.com>
2023-04-20 15:18:57 -0500

    Function Reading datos schedule

--- Parcial1_1/main.cpp
+++ Parcial1_1/main.cpp
@@ -29,3 +29,29 @@ void createCoursesAndCredits(){
 
     cout<<"creating courses and credits"<<endl;
 }
+
+void readSchedule()
+{
+    fstream fin;
+    fin.open("schedule.csv", ios::in);
+
+    char h[10], l[10], m[10], w[10], j[10], v[10], s[10];
+
+    cout << "Your schedule is:\n"
+         << endl;
+
+    while (!fin.eof())
+    {
+        fin.getline(h, 10, ',');
+        fin.getline(l, 10, ',');
+        fin.getline(m, 10, ',');
+        fin.getline(w, 10, ',');
+        fin.getline(j, 10, ',');
+        fin.getline(v, 10, ',');
+        fin.getline(s, 10, '\n');
+
+        cout << h << "\t" << l << "\t" << m << "\t" << w << "\t" << j << "\t" << v << "\t" << s << endl;
+    }
+
+    fin.close();
+}
